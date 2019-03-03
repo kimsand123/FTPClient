d0ec6f7bda0c6f899c5a7ab992c37187dd90f25a
Kim Sandberg <s@ndberg.dk>
2019-03-03 11:27:46 +0100

    Trying to get Coms up and running

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	FTP_controlprogram ftpProgram = new FTP_controlprogram();
-	ftpProgram.menu();
+	//ftpProgram.menu();
+
+	TCP_Handling tcpCom = new TCP_Handling();
+
+	tcpCom.openCommunication();
+		tcpCom.tcpSendStream("HELO\n");
+		System.out.println(tcpCom.tcpReadStream());
+	tcpCom.closeCommunication();
+
+
+
     }
 }
--- src/com/company/TCP_Handling.java
+++ src/com/company/TCP_Handling.java
@@ -9,60 +9,64 @@ class TCP_Handling {
   private Socket clientSocket;
 
   public TCP_Handling () {
+  }
+
+  public String tcpReadStream(){
+    String endResult="";
+    String line="";
+    BufferedReader inFromServer = null;
+    try {
+      inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    } catch (IOException e) {
+      System.out.println("Could not create an inputstream");
+      e.printStackTrace();
     }
-
-    public String tcpReadStream(){
-    String result="";
-      BufferedReader inFromServer = null;
+    while (!(line.equals(null))) {
       try {
-        inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-      } catch (IOException e) {
-        System.out.println("Could not create an inputstream");
-        e.printStackTrace();
-      }
-
-      try {
-        result = inFromServer.readLine();
-      } catch (IOException e) {
+        line = inFromServer.readLine();
+        endResult=endResult+line;
+        if (line.indexOf("Ok")!=-1){
+          break;
+        }
+      } catch(IOException e){
         System.out.println("Could not read stream from server");
         e.printStackTrace();
       }
-      return result;
+    }
+    return endResult;
+  }
+
+  public void tcpSendStream (String data) {
+    DataOutputStream outToServer = null;
+
+    try {
+      outToServer = new DataOutputStream(clientSocket.getOutputStream());
+    } catch (IOException e) {
+      System.out.println("Could not establish DataOutputStream");
+      e.printStackTrace();
     }
 
-    public void tcpSendStream (String data) {
-      DataOutputStream outToServer = null;
-
-      try {
-        outToServer = new DataOutputStream(clientSocket.getOutputStream());
-      } catch (IOException e) {
-        System.out.println("Could not establish DataOutputStream");
-        e.printStackTrace();
-      }
-
-      try {
-        outToServer.writeBytes(data + '\n');
-      } catch (IOException e) {
-        System.out.print("Problems writing bytestream");
-        e.printStackTrace();
-      }
+    try {
+      outToServer.writeBytes(data + '\n');
+    } catch (IOException e) {
+      System.out.print("Problems writing bytestream");
+      e.printStackTrace();
     }
+  }
 
 
-    public void openCommunication(){
-      String host = "ftp.soeborg.it";
-      int port = 21;
+  public void openCommunication(){
+    String host = "ftp.soeborg.it";
+    int port = 21;
 
-      try {
-        clientSocket = new Socket(host, port);
-      } catch (IOException e) {
-        System.out.print("Communication could not be opened");
-        e.printStackTrace();
-      }
-
-
+    try {
+      clientSocket = new Socket(host, port);
+    } catch (IOException e) {
+      System.out.print("Communication could not be opened");
+      e.printStackTrace();
     }
-  
+  }
+
 
   public void closeCommunication(){
     try {
