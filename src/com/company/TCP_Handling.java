50a17216724c27f72c3abb1ac1d0827a418fe063
Kim Sandberg <s@ndberg.dk>
2019-03-05 14:40:33 +0100

    Trying to read stream 1 char at a time.

--- src/com/company/TCP_Handling.java
+++ src/com/company/TCP_Handling.java
@@ -2,7 +2,9 @@ package com.company;
 
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.net.*;
+import java.util.stream.Stream;
 
 class TCP_Handling {
   String host;
@@ -16,19 +18,34 @@ class TCP_Handling {
 
   public String tcpReadStream(){
     StringBuilder endResult=new StringBuilder();
-    String line="";
+    String character="";
+    int length=0;
     BufferedReader inFromServer = null;
+    Stream<String> message="";
+    String[] messagearray;
 
     try {
       inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-      while ((line = inFromServer.readLine())!= null) {
-        endResult.append(line);
+      //inFromServer.mark(1);
+      message = inFromServer.lines();
+      messagearray = (String[])message.toArray();
+      int n=0;
+      while ( true) {
+
+        if (messagearray[n].equals("\\")) {
+          if ((messagearray[n+1].equals("u"))) {
+            break;
+          }
+          endResult.append(messagearray[n]);
+          n++;
+          break;
+        }
       }
+      inFromServer.close();
     } catch (IOException e) {
       System.out.println("Could not create an inputstream");
       e.printStackTrace();
     }
-
     return endResult.toString();
   }
 
