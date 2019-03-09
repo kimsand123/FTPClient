130ade46170b68f3f6f2fe81acbe89ce55479325
Kim Sandberg <s@ndberg.dk>
2019-03-09 20:10:19 +0100

    menu is cleaned

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws Exception {
     // Variables - Connection information
     String username = "ftp";
     String password = "test";
@@ -17,15 +17,6 @@ public class Main {
     String host = "ftp.soeborg.it";
     int port = 21;*/
 
-    try {
-      FTP_Client connection = new FTP_Client(username, password, host, port);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-
-    /*TEST connection = new TEST();
-    connection.connect(username, password, host, port);
-    File file = new File("c:","Testfile3.txt");
-    connection.stor(file);*/
+    FTP_Client connection = new FTP_Client(username, password, host, port);
   }
 }
